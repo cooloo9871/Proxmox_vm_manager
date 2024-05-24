2e1c5d32c800aa4c0327bb25a91a12c3cf925cbd
Andy <62133915+cooloo9871@users.noreply.github.com>
2024-05-24 17:00:33 +0800

    Update pve_vm_manager.sh

--- pve_vm_manager.sh
+++ pve_vm_manager.sh
@@ -98,10 +98,10 @@ EOF
 
   for ((z=$idstart;z<=$idend;z++))
   do
-    ssh root@"$EXECUTE_NODE" "qm create $z --name alp-$z --memory 4096 --sockets 2 --cores 2 --net0 virtio,bridge=vmbr0" &>> /tmp/pve_vm_manager.log
+    ssh root@"$EXECUTE_NODE" "qm create $z --name alp-$z --memory $MEM --sockets $CPU_socket --cores $CPU_core --net0 virtio,bridge=$Network_device" &>> /tmp/pve_vm_manager.log
     ssh root@"$EXECUTE_NODE" "qm importdisk $z /var/vmimg/nocloud_alpine-3.19.1-x86_64-bios-cloudinit-r0.qcow2 local-lvm" &>> /tmp/pve_vm_manager.log
     ssh root@"$EXECUTE_NODE" "qm set $z --scsihw virtio-scsi-pci --scsi0 local-lvm:vm-$z-disk-0" &>> /tmp/pve_vm_manager.log
-    ssh root@"$EXECUTE_NODE" "qm resize $z scsi0 50G" &>> /tmp/pve_vm_manager.log
+    ssh root@"$EXECUTE_NODE" "qm resize $z scsi0 $DISKG" &>> /tmp/pve_vm_manager.log
     ssh root@"$EXECUTE_NODE" "qm set $z --ide2 local-lvm:cloudinit" &>> /tmp/pve_vm_manager.log
     ssh root@"$EXECUTE_NODE" "qm set $z --boot c --bootdisk scsi0" &>> /tmp/pve_vm_manager.log
     ssh root@"$EXECUTE_NODE" "qm set $z --serial0 socket --vga serial0" &>> /tmp/pve_vm_manager.log
